c3865369c0b294fad6a765588c765a12d4d70398
Akarikev <princeelorm17@gmail.com>
2022-08-17 14:24:15 -0700

    new commit

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const writeEffect = document.getElementById("input");
 const button = document.getElementById("write");
+const removeTitle = document.getElementById("title");
 
 button.addEventListener("click", () => {
     console.log(writeEffect.value);
     writeEffect.style.display = "none";
     button.style.display = "none";
     document.getElementById("text").innerHTML = writeEffect.value;
+    document.getElementById("title").innerText = "";
 });
