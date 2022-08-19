d3114886c954c1daeef15a55b37218f15d163fac
Akarikev <princeelorm17@gmail.com>
2022-08-19 09:09:15 -0700

    new commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const writeEffect = document.getElementById("input");
 const button = document.getElementById("write");
 const removeTitle = document.getElementById("title");
+const emptyText = document.getElementById("write");
 
 button.addEventListener("click", () => {
     console.log(writeEffect.value);
@@ -8,4 +9,16 @@ button.addEventListener("click", () => {
     button.style.display = "none";
     document.getElementById("text").innerHTML = writeEffect.value;
     document.getElementById("title").innerText = "";
+
+    //give an alert when the text is empty
+    if (writeEffect.value == "") {
+        alert("Please write something");
+    }
+    //if text is empty, do not display an empty text, go back to the original text
+    if (writeEffect.value == "") {
+        document.getElementById("text").innerHTML = "";
+        document.getElementById("title").innerText = "";
+        writeEffect.style.display = "block";
+        button.style.display = "block";
+    }
 });
